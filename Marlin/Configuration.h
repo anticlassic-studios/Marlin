4df7143a92ba340f3c4b7dbce0581d16eba27eee
Jonathan Starr <github@jstarr.me>
2023-07-10 15:06:10 -0500

    Calibrate `X` and `Y` bed size experimentally

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1748,8 +1748,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 325
+#define Y_BED_SIZE 300
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
