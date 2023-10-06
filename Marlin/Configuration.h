52fbdb491a82f83582615107ab5ff65821292ee7
Jonathan Starr <github@jstarr.me>
2023-10-06 17:55:28 -0500

    Adjust nozzle to probe offset for new toolhead

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1537,7 +1537,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -56, 30, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 39, -6, 0 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
