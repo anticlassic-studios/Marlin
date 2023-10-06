ee66b60330a9d133311ebaa851e112fdeae60906
Jonathan Starr <github@jstarr.me>
2023-10-06 17:24:42 -0500

    Update bed size for new tool head

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1769,8 +1769,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 280
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 310
+#define Y_BED_SIZE 220
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
